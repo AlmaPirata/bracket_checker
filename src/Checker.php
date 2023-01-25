d13b64f4696526a455b24299c77f3ae5ffb255eb
Артем Воров <artem.vorov@gmail.com>
2023-01-25 21:47:11 +0200

    Initial commit

---
+++ src/Checker.php
@@ -0,0 +1,37 @@
+<?php
+
+class Checker
+{
+    private const ALLOWED_SYMBOLS = ['(', ')', ' ', '\n', '\r', '\t'];
+
+    private function doMagic(string $string): bool
+    {
+        $open = $close = 0;
+        $bracketsArr = str_split($string);
+        foreach ($bracketsArr as $symbol) {
+            if ($symbol === '(') {
+                $open++;
+            } elseif ($symbol === ')') {
+                $close++;
+            }
+        }
+
+        return $open === $close;
+    }
+
+    public function checkBrackets(string $string): bool
+    {
+        $bracketsArr = str_split($string);
+        if (count(array_diff($bracketsArr, self::ALLOWED_SYMBOLS)) > 0) {
+            throw new InvalidArgumentException('Строка содержит запрещённые символы!');
+        }
+
+        return $this->doMagic($string);
+    }
+
+}
+
+//$checker = new Checker();
+//var_dump($checker->checkBrackets('()()'));
+//var_dump($checker->checkBrackets('()()('));
+//var_dump($checker->checkBrackets('()()(1'));
